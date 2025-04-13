e229ef266e02b4aa91cec9f7839816170a0d45fe
이관용 <gy@igwan-yong-ui-MacBookAir.local>
2025-04-13 19:26:14 +0900

    customers 테이블 업로드

---
+++ customers.sql
@@ -0,0 +1,12 @@
+create table customers
+(
+customer_id char(5) not null,
+company_name varchar(30),
+manager_name varchar(20),
+manager_position varchar(20),
+city varchar(30),
+mileage int,
+phone varchar(30),
+address varchar(50),
+location varchar(50)
+);
