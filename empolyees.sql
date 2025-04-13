37c2bcc1964145d704e37d7098dad6e22e8297e0
이관용 <gy@igwan-yong-ui-MacBookAir.local>
2025-04-13 19:58:03 +0900

    empolyees 테이블 업로드

---
+++ empolyees.sql
@@ -0,0 +1,16 @@
+create table empolyees
+(
+city varchar(20),
+department_id char(2) ,
+empolyees_id char(3) not null,
+bossid_id char(3),
+birthbay date ,
+gender char(2) ,
+en_name varchar(20),
+kr_name varchar(20),
+start_date date ,
+address varchar(50),
+loacation varchar(20),
+position varchar(10) ,
+home_phone varchar(20)
+);
